e839d96fbbf7ac84df37661a3102cf65702ba371
Nicholas Choong <nicholascwc1997@gmail.com>
2021-05-08 14:06:18 +0800

    Updated forms

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField
 from wtforms.validators import DataRequired, regexp, EqualTo
-from app.models import User, Assessment, Log
+from app.models import User, Result, Log
 
 """class to hold elements and validators for login form"""
 

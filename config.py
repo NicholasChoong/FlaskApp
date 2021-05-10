f36955d3a5723cc46711127547e8792d45499855
Nicholas Choong <nicholascwc1997@gmail.com>
2021-05-11 02:28:30 +0800

    Removed `.env`

--- config.py
+++ config.py
@@ -7,10 +7,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "sshh!"
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        "DATABASE_URL"
-    ) or "sqlite:///" + os.path.join(basedir, "app.db")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "app.db")
+    # os.environ.get("DATABASE_URL") or
+    # SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class ProductionConfig(Config):

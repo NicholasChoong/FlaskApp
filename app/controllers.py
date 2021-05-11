e72857251ee787855c6c6fc88680c71a3ac02d21
Nicholas Choong <nicholascwc1997@gmail.com>
2021-05-11 13:20:47 +0800

    login now redirects to index after successful login

--- app/controllers.py
+++ app/controllers.py
@@ -16,10 +16,10 @@ class UserController:
                 flash("invalid username or password")
                 return redirect(url_for("login"))
             login_user(user, remember=form.remember_me.data)
-            next_page = request.args.get("next")
-            if not next_page or url_parse(next_page).netloc != "":
-                next_page = "index"
-            return redirect(url_for(next_page))
+            # next_page = request.args.get("next")
+            # if not next_page or url_parse(next_page).netloc != "":
+            #     next_page = "index"
+            return redirect(url_for("index"))
         return render_template("login.html", title="Sign in", form=form)
 
     def logout():

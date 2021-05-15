46f57bcb9b06c2cce278c7caf488df210e8c9e5e
Nicholas Choong <nicholascwc1997@gmail.com>
2021-05-15 22:26:42 +0800

    Added a new function

--- app/api/question_api.py
+++ app/api/question_api.py
@@ -3,3 +3,8 @@ from app.models import User, Log, Question, Attempt
 from app.api.errors import bad_request, error_response
 from flask import jsonify, url_for, request, g, abort
 from app.api.auth import token_auth
+
+@app.route("/api/user/<id>/attempt/", methods=["POST"])
+@token_auth.login_required
+def new_attempts():
+    pass

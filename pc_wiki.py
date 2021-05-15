80a9bc6f3ab42fcbde43b251733951b6b5f1d514
Nicholas Choong <nicholascwc1997@gmail.com>
2021-05-15 18:37:09 +0800

    Updated models

--- pc_wiki.py
+++ pc_wiki.py
@@ -1,7 +1,7 @@
 from app import app, db
-from app.models import User, Result, Log
+from app.models import User, Log, Question, Attempt
 
 
 @app.shell_context_processor
 def make_shell_context():
-    return {"db": db, "User": User, "Result": Result, "Log": Log}
+    return {"db": db, "User": User, "Log": Log, "Question": Question, "Attempt": Attempt}

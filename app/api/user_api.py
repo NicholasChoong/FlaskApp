4894b6414e83735d89157ba848094366af4471dc
Nicholas Choong <nicholascwc1997@gmail.com>
2021-05-15 18:38:51 +0800

    Updated imports

--- app/api/user_api.py
+++ app/api/user_api.py
@@ -1,5 +1,5 @@
 from app import app, db
-from app.models import User, Result, Log
+from app.models import User, Log, Question, Attempt
 from app.api.errors import bad_request, error_response
 from flask import jsonify, url_for, request, g, abort
 from app.api.auth import token_auth

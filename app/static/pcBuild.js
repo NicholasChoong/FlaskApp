78224141517b75523084219ae7eed4eed7444761
NICHHCIN <nichhcin@outlook.com>
2021-05-16 17:33:17 +0800

    Added Javascript to pcBuild.js

--- app/static/pcBuild.js
+++ app/static/pcBuild.js
@@ -1,5 +1,17 @@
 $(document).ready(utc2local);
 
 function utc2local() {
-    $('.UTCTime').text( (i, v) => new Date(v).toLocaleString())
+    $('.UTCTime').text( (i, v) => new Date(v).toLocaleString());
 }
+
+document.getElementById('signupPass2').addEventListener('input', (event) => {
+    const passw = document.getElementById('signupPass1').value;
+    if(passw != event.target.value){
+      event.target.classList.add('is-invalid');
+      document.getElementById('signUpButton').disabled = true;
+    } 
+    else{
+      event.target.classList.remove('is-invalid');
+      document.getElementById('signUpButton').disabled = false;
+    }
+});
